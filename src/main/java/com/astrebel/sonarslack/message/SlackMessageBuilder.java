d112b3125cbd5d254ad9a2f7836705cd5653b221
Damian Hofmann <damian.hofmann@kwsoft.ch>
2017-06-07 12:20:20 +0200

    Improve messages when title and/or reason is missing

--- src/main/java/com/astrebel/sonarslack/message/SlackMessageBuilder.java
+++ src/main/java/com/astrebel/sonarslack/message/SlackMessageBuilder.java
@@ -1,5 +1,7 @@
 package com.astrebel.sonarslack.message;
 
+import java.text.MessageFormat;
+
 public class SlackMessageBuilder {
 
 	public String build(SlackMessage message) {
@@ -18,18 +20,27 @@ public class SlackMessageBuilder {
 		builder.append(message.getShortText().replace("\n", "").replace("\r", ""));
 		builder.append("\"");
 
-		if (message.getAttachment() != null) {
-			builder.append(",\"attachments\":[");
-			builder.append(buildAttachment(message.getAttachment()));
-			builder.append("]");
-		}
+		builder.append(buildAttachment(message.getAttachment()));
 		builder.append("}");
 
 		return builder.toString();
 	}
 	
 	private String buildAttachment(SlackAttachment attachment) {
-		return "{\"text\":\"*" + attachment.getTitle() + "*\\n*Reason:*\\n" + attachment.getReasons() + "\",\"color\":\"" + attachment.getType()
-				+ "\",\"mrkdwn_in\": [\"text\"]}";
+		if (attachment == null) { return ""; }
+		
+		String title = attachment.getTitle() != null ? attachment.getTitle().trim() : "";
+		String reason = attachment.getReasons() != null ? attachment.getReasons().trim() : "";
+		String text = buildText(title, reason);
+		if (text.isEmpty()) { return ""; }
+		
+		return MessageFormat.format(",\"attachments\":['{'\"text\":\"{0}\",\"color\":\"{1}\",\"mrkdwn_in\": [\"text\"]'}']", text, attachment.getType());
+	}
+	
+	private String buildText(String title, String reason) {
+		if (!title.isEmpty() && ! reason.isEmpty()) { return MessageFormat.format("*{0}*\\n*Reason:*\\n{1}", title, reason); }
+		if (!title.isEmpty()) { return MessageFormat.format("*{0}*", title); }
+		if (!reason.isEmpty()) { return MessageFormat.format("*Reason:*\\n{0}", reason); }
+		return "";
 	}
 }
